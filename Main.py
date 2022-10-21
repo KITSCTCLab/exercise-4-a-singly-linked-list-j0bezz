00937960bf53e95de9a9ceb0430787fd9677c24e
j0bezz <109793072+j0bezz@users.noreply.github.com>
2022-10-21 15:24:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 
 from typing import Optional
 
@@ -51,14 +44,12 @@ class LinkedList:
         It prints all the elements of list.
         """
         # write code here
-        self.temp=self.head
-        while True:
-            if(self.temp.next==None):
-                print(self.temp.data)
-                break
-            else:
-                print(self.temp.data)
-                self.temp=self.temp.next
+        temp=self.head
+        list=[]
+        while temp is not None:
+            list.append(temp.data)
+            temp=temp.next
+        return list
 
 
 class Solution:
