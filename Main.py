b3f223d56360af9f5aa2397baca07ffa51088a4a
j0bezz <109793072+j0bezz@users.noreply.github.com>
2022-10-21 15:36:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
-
 from typing import Optional
 
 
 class Node:
     """
-    Provide necessary documentation
+    This class describes Node objects to act as elements of the LinkedList
+    Attributes:
+        -> data - stored associated data
+        -> next - link to next node
     """
+
     def __init__(self, data=None, next=None):
         """
-        Provide necessary documentation
+        Initialises the Node with given attributes
         """
         self.data = data
         self.next = next
@@ -16,73 +19,84 @@ class Node:
 
 class LinkedList:
     """
-    Provide necessary documentation
+    This class implements LinkedList using Node objects
+    Methods:
+        -> insert_at_end - inserts node with data at the end of the list
+        -> status - displays all elements of the lisT
+    Attributes
+        -> self.head - contains first node of LinkedList, None if list empty
     """
+
     def __init__(self):
         """
         Initialize the head
         """
         self.head = None
-        
+
     def insert_at_end(self, data):
         """
         Insert node at end of the list
         :param data: integer data that will be used to create a node
         """
-        # Write code here
-        new = Node(data,None)
-        temp=self.head
-        if temp is None:
-            self.head=new
+        new = Node(data, None)
+        current = self.head
+        if current is None:
+            self.head = new
         else:
-            while temp.next is not None:
-                temp=temp.next
-            temp.next=new
+            while current.next is not None:
+                current = current.next
+            current.next = new
 
     def status(self):
         """
         It prints all the elements of list.
         """
-        # write code here
-        temp=self.head
-        list=[]
-        while temp is not None:
-            list.append(temp.data)
-            temp=temp.next
-        print(list)
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
 class Solution:
     """
-    Provide necessary documentation
+    Class implementing functions to add numbers in a LinkedListT
+    
     """
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
         """
         :param first_list: Linkedlist with non-negative integers
         :param second_list: Linkedlist with non-negative integers
         :return: returns the sum as a linked list
         """
-        # Write code here
-        result= self.getnun(first_list)+self.getnun(second_list)
-        sum=LinkedList()
-        for digit in list(map(int,str(result)[::-1])):
-            sum.insert_at_end(digit)
-        return sum
-        
-    def getnun(self,l: Optional[LinkedList]) -> int:        
-        temp=l.head
-        num=""
-        if temp is None:
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        """
+        :param l: LinkedList with non-negative integers
+        :return: returns digits of the list as a single integer
+        """
+        curr = l.head
+        if curr is None:
             return 0
-        
-        else:
-            num=str(temp.data)+num
-            temp=temp.next
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
         return int(num)
-# Do not edit the following code      
+
+
+# Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
-# Create an another instance for LinkedList
+# Create an another instance for LinkedListT
 second_list = LinkedList()
 # Read data for first list
 data_for_first_list = list(map(int, input().strip().split(" ")))
@@ -100,5 +114,3 @@ solution = Solution()
 new_list = solution.addTwoNumbers(first_list, second_list)
 # Display the status of new_list
 new_list.status()
-
-
